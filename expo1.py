f971fa0dd7d1368553bd6d49e5fe2d2300afd5a9
thephysist <kumbiraimagura120@gmail.com>
2025-02-24 21:40:03 +0200

    third comment

--- expo1.py
+++ expo1.py
@@ -4,3 +4,9 @@ from operator import add
 g=add(9,9)
 print(g*8)
 print("We have officially made it")
+a=5
+b=3
+print(a,b)
+r="a"+"a"
+print(r)
+print("#Effective notekeeping in the real world is an essential element")
